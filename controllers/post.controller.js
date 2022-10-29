ea9fa6e4feece4f86ee73ec4159fd65f9f4bf778
Francis Lukoki <francislukoki@hotmail.fr>
2022-10-29 11:53:49 +0200

    mise en place des posts antéchronologique

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -9,7 +9,7 @@ exports.readPost = (req, res) => {
     postModel.find((err, docs) => {
         if (!err) res.send(docs);
         else console.log('Error to get data : ' + err);
-    })
+    }).sort({ createdAt: -1 })
 }
 
 
